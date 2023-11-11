4c5dcd1ab1d4faad78966998aa6e41b3a2e061f3
alecnazworth <alecnazworthalt@gmail.com>
2023-11-11 17:45:30 -0500

    -added customize column for changing colors and adding sections -updated css

---
+++ scripts/customize.js
@@ -0,0 +1,28 @@
+function changeColor(color) {
+  $("#resume-header").css("background-color", color);
+  $("#exp-header").css("border-bottom", "1px solid" + color);
+  $("#exp-header").css("color", color);
+  $("#edu-header").css("border-bottom", "1px solid" + color);
+  $("#edu-header").css("color", color);
+  $("#skills-header").css("border-bottom", "1px solid" + color);
+  $("#skills-header").css("color", color);
+}
+
+function addExp() {
+  $("#exp").append(
+    '<textarea name="title" id="job-title" cols="1" rows="1" placeholder="Job title"></textarea><textarea name="date" id="job-date" cols="1" rows="1" placeholder="Starting date - end date"></textarea><textarea name="job description" id="job-desc" cols="30" rows="3" placeholder="Job description"></textarea>'
+  );
+}
+
+function addEdu() {
+  // add another edu-item div to the #edu div
+  $("#edu").append(
+    '<textarea name="school" id="edu-title" cols="1" rows="1" placeholder="School name"></textarea><textarea name="date" id="edu-date" cols="1" rows="1" placeholder="Starting date - end date"></textarea><textarea name="description" id="edu-desc" cols="30" rows="3" placeholder="Education description"></textarea>'
+  );
+}
+
+function addSkill() {
+  $("#skills ul").append(
+    '<li><textarea name="skill" id="skill2" cols="30" rows="1" placeholder="Skill"></textarea></li>'
+  );
+}
